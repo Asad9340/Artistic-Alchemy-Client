a1ec164c1be081d34cb9ca65d35d2d900ab09ae8
Asad9340 <asadulimran1999@gmail.com>
2024-04-26 17:54:14 +0600

    added error page

--- src/Routes/Route.jsx
+++ src/Routes/Route.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../Layout/Root';
-import Error from '../Pages/Error/Error';
 import Homepage from '../Pages/Home/Homepage';
 import SignIn from '../Pages/SignIn/SignIn';
 import Signup from '../Pages/Signup/Signup';
@@ -9,6 +8,7 @@ import Details from '../components/Details/Details';
 import AllArtCraft from '../components/AllArtCraft/AllArtCraft';
 import AddCraft from '../components/AddCraft/AddCraft';
 import MyArtCraft from '../components/MyArtCraft/MyArtCraft';
+import Error from './../Pages/Error/Error';
 const router = createBrowserRouter([
   {
     path: '/',
@@ -17,39 +17,45 @@ const router = createBrowserRouter([
     children: [
       {
         path: '/',
-        element:<Homepage/>
+        element: <Homepage />,
       },
       {
         path: '/signin',
-        element:<SignIn/>
+        element: <SignIn />,
       },
       {
         path: '/signup',
-        element:<Signup/>
+        element: <Signup />,
       },
       {
         path: '/details',
-        element: <PrivateRoute>
-          <Details/>
-        </PrivateRoute>
+        element: (
+          <PrivateRoute>
+            <Details />
+          </PrivateRoute>
+        ),
       },
       {
         path: '/allArtCraft',
-        element:<AllArtCraft/>,
+        element: <AllArtCraft />,
       },
       {
         path: '/addCraft',
-        element: <PrivateRoute>
-          <AddCraft/>
-        </PrivateRoute>
+        element: (
+          <PrivateRoute>
+            <AddCraft />
+          </PrivateRoute>
+        ),
       },
       {
         path: '/myArtCraft',
-        element: <PrivateRoute>
-          <MyArtCraft/>
-        </PrivateRoute>
+        element: (
+          <PrivateRoute>
+            <MyArtCraft />
+          </PrivateRoute>
+        ),
       },
-    ]
+    ],
   },
 ]);
 
--- src/components/AllArtCraft/AllArtCraft.jsx
+++ src/components/AllArtCraft/AllArtCraft.jsx
@@ -1,5 +1,13 @@
+import { useEffect } from "react"
 
 function AllArtCraft() {
+  useEffect(() => {
+    (async () => {
+      const res = await fetch('http://localhost:5000/allArtCraft');
+      const data = await res.json();
+      console.log(data);
+    })()
+  },[])
   return (
     <div>AllArtCraft</div>
   )
